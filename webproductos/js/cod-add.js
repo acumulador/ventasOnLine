ceabcaf510f418d82e7aecc24de5eecf5178a039
Juan Carlos Salazar <jcsalazarm@gmail.com>
2017-05-11 11:41:40 -0500

    Avance en agregar productos, tiene error

--- webproductos/js/cod-add.js
+++ webproductos/js/cod-add.js
@@ -2,5 +2,49 @@ $(document).on("ready", ini);
 
 function ini()
 {
-	alert("Agregar producto!!");
+	$("#file-img").fileinput({
+		allowedFileExtensions: ['jpg', 'png', 'gif'],
+        showUpload: false,
+        maxFileCount: 1,
+        mainClass: "input-group-lg"
+    });
+
+    $('#btnEnviar').on("click", enviarProducto)
+
+}
+
+function enviarProducto()
+{
+	try
+    {
+        if($('#txtTitulo').val() != "" && $('#txtDetalle').val() != "")
+        {
+            
+            $.ajax({
+                type: "POST",
+                dataType: "json",
+                data: dataProd,
+                url: 'http://localhost/apiproductos/productos/',
+                success: function(data) {
+                    alert("Producto registrado correctamente!!");
+
+                    $('#txtTitulo').val("");
+                    $('#txtDetalle').val("");
+                    $('#file-img').val("");
+
+                },
+                error: function(ex) {
+                    alert("Error obteniendo datos!! " + ex);
+                }
+            });
+        }
+        else
+        {
+            alert("Ingrese todos los campos!!");
+        }
+    }
+    catch(ex)
+    {
+        alert("Error de datos!!");
+    }
 }
