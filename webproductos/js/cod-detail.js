158b20fce003879a851c2cba314024b739c36a01
Juan Carlos Salazar <jcsalazarm@gmail.com>
2017-05-11 10:55:10 -0500

    Se agregan estilos css, se agrega funcionalidad para consultar y visualizar detalle de producto, se completa vista de detalle

--- webproductos/js/cod-detail.js
+++ webproductos/js/cod-detail.js
@@ -1,6 +1,44 @@
 $(document).on("ready", ini);
 
+var oID = localStorage.getItem("var");
+
 function ini()
 {
-	
+	if (oID === null)
+	{
+		window.location.href = 'index.html';
+	}
+
+	consultarProd(oID);
+}
+
+
+function consultarProd(id)
+{
+	try
+    {
+        $.ajax({
+            type: "GET",
+            dataType: "json",
+            url: 'http://localhost/apiproductos/productos/'+id,
+            success: function(data) {
+
+            	//alert(data.productos[1].foto);
+            	var fuente = $('#plantillaDetalle').html();
+				var plantillaDetalle = Handlebars.compile(fuente);
+
+            	var datos = data.producto;
+            	var html = plantillaDetalle(datos);
+            	$('#detalle').html(html);
+            },
+            error: function() {
+            	alert("Error obteniendo datos!!");
+            }
+        });
+
+    }
+    catch(ex)
+    {
+        alert("Error de datos!!");
+    }
 }
