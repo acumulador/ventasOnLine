773fa08215dec094164e81d2c5cc40f545f36c2f
Juan Carlos Salazar <jcsalazarm@gmail.com>
2017-05-11 10:27:37 -0500

    Se agrega vista para listado y su respectivo codigo js, pruebas de listado en local, se consume api rest local

--- webproductos/js/cod-ppal.js
+++ webproductos/js/cod-ppal.js
@@ -2,5 +2,45 @@ $(document).on("ready", ini);
 
 function ini()
 {
-	alert("Funciona!!");
+	traerDatos();
+	$(document).on("click", ".goRow", showDetail);
+}
+
+
+function showDetail()
+{
+	var oID = $(this).attr("id");
+	//imgSel = $(this).attr("src");
+	localStorage.setItem("var", oID);
+
+	window.location.href = 'detalle.html';
+}
+
+function traerDatos()
+{
+    try
+    {
+        $.ajax({
+            type: "GET",
+            dataType: "json",
+            url: 'http://localhost/apiproductos/productos/',
+            success: function(data) {
+
+            	var fuente = $('#plantilla').html();
+				var plantilla = Handlebars.compile(fuente);
+
+            	var datos = data.productos;
+            	var html = plantilla(datos);
+            	$('#fila').html(html);
+            },
+            error: function() {
+            	alert("Error obteniendo datos!!");
+            }
+        });
+
+    }
+    catch(ex)
+    {
+        alert("Error de datos!!");
+    }
 }
